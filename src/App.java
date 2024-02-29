6fed575ec91533f243119ed5bcd22d1b328b6bb1
RobertSouDev <rogerhallks1@gmail.com>
2024-02-29 15:59:21 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero1 = sc.nextInt();
+
+        System.out.print("Digite um numero: ");
+        int numero2 = sc.nextInt();
+
+        System.out.print("Digite um numero: ");
+        int numero3 = sc.nextInt();
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+
+    
+    }
+}
